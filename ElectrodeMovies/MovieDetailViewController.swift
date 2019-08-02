242675deb7ec73901fc946d496fb7bef25f0873c
Alan Paiva <alan.jeferson11@gmail.com>
2019-08-01 23:20:59 -0300

    Add mock request handler to movie list

---
+++ ElectrodeMovies/MovieDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailViewController.swift
+//  ElectrodeMovies
+//
+//  Created by Alan Jeferson on 8/1/19.
+//  Copyright © 2019 Alan Paiva. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+}
--- ElectrodeMovies/ViewController.swift
+++ ElectrodeMovies/ViewController.swift
@@ -15,9 +15,38 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     let movieListMiniAppViewController = ElectrodeReactNative.sharedInstance().miniApp(withName: "MovieListMiniApp", properties: nil)
-    movieListMiniAppViewController.view.frame = view.bounds
-    view.addSubview(movieListMiniAppViewController.view)
+    let movieListView = movieListMiniAppViewController.view as UIView
+    view.addSubview(movieListView)
+    movieListView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: movieListView.topAnchor),
+      view.bottomAnchor.constraint(equalTo: movieListView.bottomAnchor),
+      view.leadingAnchor.constraint(equalTo: movieListView.leadingAnchor),
+      view.trailingAnchor.constraint(equalTo: movieListView.trailingAnchor)
+      ])
     movieListMiniAppViewController.didMove(toParent: self)
+    
+    ElectrodeBridgeHolder.registerRequestHandler(withName: "br.com.foo") { (data, completion) in
+      let arr = [
+        [
+          "id": "3",
+          "title": "Spider Man: Far From Home",
+          "year": "2019",
+          "image": [
+            "uri": "https://image.tmdb.org/t/p/w1280/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg"
+          ]
+        ],
+        [
+          "id": "4",
+          "title": "The Angry Birds Movie 2",
+          "year": "2019",
+          "image": [
+            "uri": "https://image.tmdb.org/t/p/w1280/hdYTrfRKrzdbZ3DE72YxmeB0RNg.jpg"
+          ]
+        ]
+      ];
+      completion(arr, nil)
+    }
   }
 }
 
